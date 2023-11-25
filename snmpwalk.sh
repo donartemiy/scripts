364deee88af59d4058470e7db91d77c9df4ddc80
Artemiy <31482424+donartemiy@users.noreply.github.com>
2023-11-25 09:46:09 +0500

    Update snmpwalk.sh

--- snmpwalk.sh
+++ snmpwalk.sh
@@ -2,55 +2,12 @@
 # description sw-core(peripherals)
 # description dts-sw-dl-mp33-peripherals
 echo "dts-sw-dl-mp33-peripherals"
-snmpwalk -v 2c -c public 10.177.96.25 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.96.26 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.99.97 1.3.6.1.2.1.1.3
+snmpwalk -v 2c -c public 10.0.1.25 1.3.6.1.2.1.1.3
+snmpwalk -v 2c -c public 10.0.1.26 1.3.6.1.2.1.1.3
+snmpwalk -v 2c -c public 10.0.1.97 1.3.6.1.2.1.1.3
 
 # description dts-sw-dl-n20-peripherals
 echo "dts-sw-dl-n20-peripherals"
-snmpwalk -v 2c -c public 10.177.96.33 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.96.34 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.98.1 1.3.6.1.2.1.1.3
-
-# description dts-sw-dl-s97-peripherals
-echo "dts-sw-dl-s97-peripherals"
-snmpwalk -v 2c -c public 10.177.96.41 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.96.42 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.99.1 1.3.6.1.2.1.1.3
-
-# description dts-sw-dl-u36-peripherals
-echo "dts-sw-dl-u36-peripherals"
-snmpwalk -v 2c -c public 10.177.96.49 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.96.50 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.1 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.3 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.4 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.10 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.11 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.12 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.13 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.14 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.15 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.16 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.17 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.18 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.22 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.33 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.134 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.135 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.136 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.138 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.139 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.140 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.141 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.143 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.144 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.146 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.97.148 1.3.6.1.2.1.1.3
-
-# description dts-sw-dl-u48-peripherals
-echo "dts-sw-dl-u48-peripherals"
-snmpwalk -v 2c -c public 10.177.96.57 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.96.58 1.3.6.1.2.1.1.3
-snmpwalk -v 2c -c public 10.177.99.65 1.3.6.1.2.1.1.3
-
+snmpwalk -v 2c -c public 10.0.2.33 1.3.6.1.2.1.1.3
+snmpwalk -v 2c -c public 10.0.2.34 1.3.6.1.2.1.1.3
+snmpwalk -v 2c -c public 10.0.2.1 1.3.6.1.2.1.1.3
