8a61473217f4497cce72b76bf923e107ec462867
Artemiy <31482424+donartemiy@users.noreply.github.com>
2023-12-13 07:22:26 +0500

    Update one_liners.sh

--- one_liners.sh
+++ one_liners.sh
@@ -2,3 +2,6 @@
 
 # delete files which has specific size
 $ sudo find . -maxdepth 1 -xdev -type f -size +1M -size -3M -delete
+
+# save result of ping
+$ ping www.google.fr | while read pong; do echo "$(date): $pong"; done
