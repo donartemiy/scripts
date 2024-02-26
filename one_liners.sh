2e6274c4d1832bd8ff0711f0010115ebc745a3e7
Artemiy <31482424+donartemiy@users.noreply.github.com>
2024-02-26 17:18:21 +0500

    Update one_liners.sh

--- one_liners.sh
+++ one_liners.sh
@@ -5,3 +5,6 @@ $ sudo find . -maxdepth 1 -xdev -type f -size +1M -size -3M -delete
 
 # save result of ping
 $ ping www.google.fr | while read pong; do echo "$(date): $pong"; done
+
+# Uninstall docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
